46517ed2ccb59a97747ee1c31f4737f4a5635e53
Lukeveille <luke@lukeleveille.com>
2018-10-22 23:49:28 -0400

    completed exercise

---
+++ dictionary.rb
@@ -0,0 +1,23 @@
+class Dictionary
+  def initialize
+    @entries = {}
+  end
+
+  def add(hash)
+    hash.class == Hash ? @entries.merge!(hash) : @entries.merge!(hash => nil)
+  end
+
+  def keywords
+    @entries.keys.sort
+  end
+
+  def include?(value)
+    return true if @entries.include?(value)
+  end
+
+  def find(value)
+    selections = self.entries.select { |k,v| k[0..(value.length - 1)] == value }
+  end
+
+  attr_accessor :entries
+end
